8903c876c67a90cdb1a0d42296468f6f0efed2b8
kefo-tech <kefo.tech@gmail.com>
2026-04-05 17:41:52 +0200

    Update sw.js

--- map/sw.js
+++ map/sw.js
@@ -1,4 +1,4 @@
-const CACHE = "complaints-mvp-v8";
+const CACHE = "complaints-mvp-v9";
 const ASSETS = ["./", "./index.html", "./styles.css", "./app.js", "./manifest.json"];
 
 self.addEventListener("install", (e) => {
